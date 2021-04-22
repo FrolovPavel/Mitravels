e2a6300a81316f8ff11858ba0fa93b50c686bc27
Pavel Frolov <Frolov-Works@yandex.ru>
2021-04-23 00:25:06 +0300

    main layout rdy

---
+++ src/js/menu.js
@@ -0,0 +1,11 @@
+let burger = document.querySelector('.burger'),
+    close = document.querySelector('.menu__close');
+    menu = document.querySelector('.menu');
+
+burger.addEventListener('click', () => {
+    menu.classList.add('menu--visible')
+})
+
+close.addEventListener('click', () => {
+    menu.classList.remove('menu--visible')
+})
--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,9 +1,21 @@
 import Swiper from 'swiper';
 import 'swiper/swiper-bundle.css';
 
-const swiper = new Swiper('.slider-container', {
+const swiper = new Swiper('.swiper-container', {
+    centeredSlides: true,
+    slidesPerView: 1,
+    loop: true,
+    spaceBetween: 10,
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
+});
+
+
+const swiper2 = new Swiper('.slider-container', {
     centeredSlides: true,
     slidesPerView: 'auto',
     loop: true,
-    spaceBetween: 105
+    spaceBetween: 105,
 });
